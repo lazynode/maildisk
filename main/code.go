9f2b8355df88af3bee5103c754eefa8f6e23b405
Your Name <you@example.com>
2022-12-12 05:50:35 +0000

    dev

--- main/code.go
+++ main/code.go
@@ -37,15 +37,5 @@ func main() {
 		}))
 	default:
 		panic(`invalid cmd`)
-		// case "put-value":
-		// 	fmt.Println(hex.EncodeToString(mail.PutValue(lazy.Unwrap(os.ReadFile(lazy.Default("/dev/stdin")(os.LookupEnv(`IF`)))))))
-		// case "get-value":
-		// 	lazy.Assert(os.WriteFile(lazy.Default("/dev/stdout")(os.LookupEnv(`OF`)), mail.GetValue(lazy.Unwrap(hex.DecodeString(lazy.Default("")(os.LookupEnv(`HASH`))))), 0644))
-		// case "put-object":
-		// 	fmt.Println(hex.EncodeToString(mail.PutObject(lazy.Unwrap(os.ReadFile(lazy.Default("/dev/stdin")(os.LookupEnv(`IF`)))))))
-		// case "get-object":
-		// 	lazy.Assert(os.WriteFile(lazy.Default("/dev/stdout")(os.LookupEnv(`OF`)), mail.GetObject(lazy.Unwrap(hex.DecodeString(lazy.Default("")(os.LookupEnv(`HASH`))))), 0644))
-		// default:
-		// 	panic("invalid command")
 	}
 }
